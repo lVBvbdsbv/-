3dc7f12304e7d51eefc65c56a4637a501d718f7a
Kostya <46630474+lVBvbdsbv@users.noreply.github.com>
2024-09-11 11:36:30 +0400

    Add files via upload

---
+++ Calculator2/Calculator2/Form1.cs
@@ -0,0 +1,20 @@
+namespace Calculator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Student\\source\\repos\\Calculator1\\Calculator1\\Новая папка\\56229.600.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Student\\source\\repos\\Calculator1\\Calculator1\\Новая папка\\966.970.jpg");
+        }
+    }
+}
